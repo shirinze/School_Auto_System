1c56acf9ee9dc8ad22519d2223b872ea69e2de2c
shirinze <sherize1373@gmail.com>
2024-05-16 12:18:25 +0300

    teacher login

--- School_WebUI/Controllers/AccountController.cs
+++ School_WebUI/Controllers/AccountController.cs
@@ -1,12 +1,84 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using School.Core.Service;
+using School.Model.Entities;
+using School_WebUI.Models.ViewModels;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace School_WebUI.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly ICoreService<Teacher> _teach;
+        private readonly ICoreService<Student> _stu;
+        public AccountController(ICoreService<Teacher> teach,ICoreService<Student> stu)
+        {
+            _teach = teach;
+            _stu = stu;
+        }
+      
         public IActionResult Login()
         {
             return View();
         }
+        [HttpPost]
+        // Metodu Asenkron metod olarak tanımladık. Asenkron metodlar aynı anda birden fazla işleme cevap verebilirler.
+
+        public async Task<IActionResult> Login(LoginViewModel lvm)
+        {
+            if(lvm.LoginType==1) //teacher
+            {
+                
+                var result = _teach.GetRecord(x => x.TeacherName == lvm.name && x.TeacherSurname == lvm.surename);
+                if(result!=null)
+                {
+                    //claims(talepler)
+                    var claims = new List<Claim>()
+                    {
+                        new Claim("ID",result.ID.ToString()),
+                        new Claim("LoginType",lvm.LoginType.ToString()),
+                        new Claim(ClaimTypes.Name,result.TeacherName),
+                        new Claim(ClaimTypes.Surname,result.TeacherSurname)
+                        
+                    };
+
+                    //identity
+                    var user = new ClaimsIdentity(claims,"Login");
+
+                    //prnsip
+
+                    ClaimsPrincipal principal = new ClaimsPrincipal(user);
+                    //login
+                    await HttpContext.SignInAsync(principal);
+                    return RedirectToAction("Index", "Teacher", new { area = "User" });
+                    
+                }
+
+            }
+            else if(lvm.LoginType==2) //student
+            {
+                var result = _stu.GetRecord(x => x.Name == lvm.name && x.SureName == lvm.surename);
+                if(result!=null)
+                {
+                    var claims = new List<Claim>()
+                    { 
+                        new Claim("ID",result.ID.ToString()),
+                        new Claim("LoginType",lvm.LoginType.ToString()),
+                        new Claim(ClaimTypes.Name,result.Name),
+                        new Claim(ClaimTypes.Surname,result.SureName)
+                    };
+
+                    var user = new ClaimsIdentity(claims, "Login");
+                    ClaimsPrincipal principal = new ClaimsPrincipal(user);
+                    await HttpContext.SignInAsync(principal);
+                    return RedirectToAction("Index", "Student", new { area = "User" });
+                }
+            }
+            return View();
+        }
+
+      
     }
 }
---
+++ School_WebUI/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace School_WebUI.Models.ViewModels
+{
+    //eger viewden controllere veri aktarmak gerekirse viewmodel deriz!
+    public class LoginViewModel
+    {
+        public string name { get; set; }
+        public string surename { get; set; }
+        public int LoginType { get; set; }
+    }
+}
