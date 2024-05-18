78e7c4d8216c2c2809abcc948e9004fd28a0da5e
shirinze <sherize1373@gmail.com>
2024-05-18 15:02:05 +0300

    added teacher area and student note

--- School_WebUI/Areas/User/Controllers/TeacherController.cs
+++ School_WebUI/Areas/User/Controllers/TeacherController.cs
@@ -15,12 +15,18 @@ namespace School_WebUI.Areas.User.Controllers
         private readonly ICoreService<Teacher> _teacherdb;
         private readonly ICoreService<Lesson> _lessondb;
         private readonly ICoreService<Student> _studentdb;
-        public TeacherController(ICoreService<Teacher> teacherdb,ICoreService<Lesson> lessondb,ICoreService<Student> studentdb)
+        private readonly ICoreService<Note> _notedb;
+        public TeacherController(ICoreService<Teacher> teacherdb,ICoreService<Lesson> lessondb,ICoreService<Student> studentdb,ICoreService<Note> notedb)
         {
             _teacherdb = teacherdb;
             _lessondb = lessondb;
             _studentdb = studentdb;
+            _notedb = notedb;
+
         }
+           
+            
+        
         public IActionResult Index()
         {
             //ogretmene ait id bilgisi
@@ -45,6 +51,58 @@ namespace School_WebUI.Areas.User.Controllers
         {
             return View(_studentdb.GetAll());
         }
+        public IActionResult NoteAdd(int id)
+        {
+            return View(_studentdb.GetById(id));
+        }
+        [HttpPost]
+        public IActionResult NoteAdd(Student s,int score,int ders)
+        {
+            //ilk once boyle bir kayit var mi diye bakacagiz onun icin note ait eklemeler yapilacak
+            var kayit = _notedb.GetRecord(x => x.StudentId == s.ID && x.LessonId == ders);
+            if(kayit==null)
+            {
+                //note sinifina ait nesne 
+                var record = new Note()
+                {
+                    Score = score,
+                    StudentId=s.ID,
+                    LessonId=ders
+                };
+                return _notedb.Add(record) ? View("StudentList", _studentdb.GetAll()) : View();
+
+            }
+            return View("StudentList", _studentdb.GetAll());
+
+
+
+            
+        }
+
+        public IActionResult NoteEdite(int id)
+        {
+            var result = _notedb.GetRecord(x => x.StudentId==id );
+            if(result!=null)
+            {
+                var studentnote = _studentdb.GetById(result.StudentId);
+                var lessonnote = _lessondb.GetById(result.LessonId);
+
+                var record = new StudentNoteDto
+                {
+                    studentid=result.StudentId,
+                    lessonid=result.LessonId,
+                    score=result.Score,
+                    name=studentnote.Name,
+                    surename=studentnote.SureName,
+                    Lessons=_lessondb.GetAll()
+
+                };
+                return View(record);
+            }
+            return View();
+        }
+
+
 
     }
 }
---
+++ School_WebUI/Areas/User/Models/Dtos/StudentNoteDto.cs
@@ -0,0 +1,16 @@
+﻿using School.Model.Entities;
+using System.Collections.Generic;
+
+namespace School_WebUI.Areas.User.Models.Dtos
+{
+    public class StudentNoteDto
+    {
+        public int studentid { get; set; }
+        public int lessonid { get; set; }
+        public int score { get; set; }
+        public string name { get; set; }
+        public string surename { get; set; }
+        public List<Lesson> Lessons { get; set; }
+
+    }
+}
